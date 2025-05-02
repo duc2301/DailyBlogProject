27bfc31c9834b243713c0e3f31d1a19d716e98ad
duc2301 <phamvanduc2301@gmail.com>
2025-05-02 20:47:48 +0700

    Fix Router: Name Link

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../Pages/HomePage.jsx';
-import Login from '../Pages/LoginPage.jsx'
-import Signup from '../Pages/SignUpPage.jsx'
+import Home from '../Pages/HomePage/Index.jsx';
+import Login from '../Pages/LoginPage/Index.jsx'
+import Signup from '../Pages/SignUpPage/Index.jsx'
 
 const AppRouter = () => {
   return (
